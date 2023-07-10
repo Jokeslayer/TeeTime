63dfea97cb4b4e900957e79345883f20437c9674
Jokeslayer <aschwartz07901@gmail.com>
2023-07-10 16:59:35 -0600

    Created models for course and reservation objects

---
+++ models/course.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  userName: String,
+  userAvatar: String
+}, {
+  timestamps: true
+});
+
+const CourseSchema = new Schema({
+    Name: { type: String, required: true },
+    reviews: [reviewSchema]
+  }, {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  });
+  
+  
+  CourseSchema.virtual('rating')
+    .get(function () {
+      if (this.reviews.length === 0) return 0;
+      let count = 0;
+      this.reviews.forEach(function (r) {
+        count += r.rating
+      });
+      return count / this.reviews.length;
+    })
+  
+  // Compile the schema into a model and export it
+  module.exports = mongoose.model('Course', CourseSchema);
---
+++ models/reservation.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+    timeSlot: {type: String},
+    date: { type: Date },
+    cart: { type: Boolean },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    userName: String,
+    userAvatar: String,
+    roundLength: {type: Number,
+        enum: [9,18],
+        default: 18
+    },
+    price: {type: Number}
+  }, {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  });  
+  
+  // Compile the schema into a model and export it
+  module.exports = mongoose.model('Reservation', reservationSchema);
