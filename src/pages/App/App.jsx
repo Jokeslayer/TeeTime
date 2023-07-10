d2fd84ea38969d0f2a30777567d4d0c70af963fd
Jokeslayer <aschwartz07901@gmail.com>
2023-07-10 15:55:36 -0600

    Created the react skeleton. Components and Pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,8 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import SchedulerPage from '../SchedulerPage/SchedulerPage';
+import CourseInfoPage from '../CourseInfoPage/CourseInfoPage';
+import ReviewPage from '../ReviewPage/ReviewPage';
+import AccountPage from '../AccountPage/AccountPage';
+
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -17,8 +20,9 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/courses" element={<CourseInfoPage />} />
+              <Route path="/scheduler" element={<SchedulerPage />} />
+              <Route path="/account" element={<AccountPage />} />
             </Routes>
           </>
           :
---
+++ src/pages/CourseInfoPage/CourseInfoPage.jsx
@@ -0,0 +1,3 @@
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import * as ordersAPI from '../../utilities/orders-api';
